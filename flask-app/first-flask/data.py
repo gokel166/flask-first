eccff6f7004d9ea18b5ee3462dead004b013e272
gokel166 <okellogeorge40@yahoo.com>
2017-05-05 09:36:17 -0500

    First python commit

---
+++ flask-app/first-flask/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Here is the full scoop',
+            'author': 'George Okello',
+            'create_date': '03-21-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Here is the full scoop 2',
+            'author': 'George Okello',
+            'create_date': '03-21-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article Two',
+            'body': 'Here is the full scoop 2',
+            'author': 'George Okello',
+            'create_date': '03-21-2017'
+        }
+    ]
+    return articles
